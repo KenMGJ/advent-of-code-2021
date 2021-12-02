d9e6bba16fc640456b817b20aed04f6575fa219e
Ken Giroux <kenmgj@yahoo.com>
2021-12-02 07:38:13 -0500

    day 2 complete

--- internal/runner/day02.go
+++ internal/runner/day02.go
@@ -1,13 +1,62 @@
 package runner
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func (r *Runner) Day02Part1(lines []string) {
 
-	fmt.Println(0)
+	h, d := 0, 0
+
+	for _, l := range lines {
+
+		c, v := lineToCommandValuePair(l)
+
+		switch c {
+		case "forward":
+			h += v
+		case "down":
+			d += v
+		case "up":
+			d -= v
+		}
+	}
+
+	fmt.Println(h * d)
 }
 
 func (r *Runner) Day02Part2(lines []string) {
 
-	fmt.Println(0)
+	h, d, aim := 0, 0, 0
+
+	for _, l := range lines {
+
+		c, v := lineToCommandValuePair(l)
+
+		switch c {
+		case "forward":
+			h += v
+			d += aim * v
+		case "down":
+			aim += v
+		case "up":
+			aim -= v
+		}
+	}
+
+	fmt.Println(h * d)
+}
+
+func lineToCommandValuePair(line string) (string, int) {
+
+	sl := strings.Split(line, " ")
+	c := sl[0]
+	v, err := strconv.Atoi(sl[1])
+	if err != nil {
+		panic(err)
+	}
+
+	return c, v
 }
